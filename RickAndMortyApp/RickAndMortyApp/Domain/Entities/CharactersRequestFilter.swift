2884434420b50af881f5b5e969cd64b4d4256e7d
José María Márquez Crespo <josemaria.marquez@ust.com>
2024-09-03 13:38:56 +0200

    refactor: update characters request filter file name

---
+++ RickAndMortyApp/RickAndMortyApp/Domain/Entities/CharactersRequestFilter.swift
@@ -0,0 +1,29 @@
+//
+//  CharactersRequestFilterDTO.swift
+//  RickAndMortyApp
+//
+//  Created by José María Márquez Crespo on 3/9/24.
+//
+
+import Foundation
+
+struct CharactersRequestFilter {
+    let gender: String?
+}
+
+extension CharactersRequestFilter {
+
+    func buildFilters() -> String {
+        var filterComponents: [String] = []
+
+        if let gender = gender {
+            filterComponents.append(buildFilterComponent(name: "gender", value: gender))
+        }
+        
+        return filterComponents.isEmpty ? "" : "filter: { \(filterComponents.joined(separator: ", ")) }"
+    }
+
+    private func buildFilterComponent(name: String, value: String) -> String {
+        return "\(name): \"\(value)\""
+    }
+}
