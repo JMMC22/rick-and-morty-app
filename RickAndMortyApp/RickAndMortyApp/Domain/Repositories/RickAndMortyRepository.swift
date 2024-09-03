09969aeb5653d1c259bb23677f5dd1cc1b0ad44a
José María Márquez Crespo <josemaria.marquez@ust.com>
2024-09-03 11:48:21 +0200

    feat: add rick and morty repository interface

---
+++ RickAndMortyApp/RickAndMortyApp/Domain/Repositories/RickAndMortyRepository.swift
@@ -0,0 +1,12 @@
+//
+//  RickAndMortyRepository.swift
+//  RickAndMortyApp
+//
+//  Created by José María Márquez Crespo on 3/9/24.
+//
+
+import Foundation
+
+protocol RickAndMortyRepository {
+    func fetchCharacters(page: Int) -> Result<[RickAndMortyCharacter], RequestError>
+}
