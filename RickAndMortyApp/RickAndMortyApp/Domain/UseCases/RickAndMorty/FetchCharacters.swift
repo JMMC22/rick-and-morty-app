3d530c7a81a866b95df9dcc1161b0b42d7bd90fb
José María Márquez Crespo <josemaria.marquez@ust.com>
2024-09-03 12:02:54 +0200

    feat: add fetch characters use case

---
+++ RickAndMortyApp/RickAndMortyApp/Domain/UseCases/RickAndMorty/FetchCharacters.swift
@@ -0,0 +1,28 @@
+//
+//  FetchCharacters.swift
+//  RickAndMortyApp
+//
+//  Created by José María Márquez Crespo on 3/9/24.
+//
+
+import Foundation
+
+protocol FetchCharacters {
+    func execute(page: Int) async -> Result<[RickAndMortyCharacter], RequestError>
+}
+
+class DefaultFetchCharacters {
+
+    private let rickAndMortyRepository: RickAndMortyRepository
+
+    init(rickAndMortyRepository: RickAndMortyRepository) {
+        self.rickAndMortyRepository = rickAndMortyRepository
+    }
+}
+
+extension DefaultFetchCharacters: FetchCharacters {
+
+    func execute(page: Int = 1) async -> Result<[RickAndMortyCharacter], RequestError> {
+        return await rickAndMortyRepository.fetchCharacters(page: page)
+    }
+}
