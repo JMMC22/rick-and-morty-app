20e9e57e57362dcbee17440aa85e541a612ddb8c
José María Márquez Crespo <josemaria.marquez@ust.com>
2024-09-03 10:30:07 +0200

    initial commit

---
+++ RickAndMortyApp/RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by José María Márquez Crespo on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
