e79a57f84326a8da5c6258736e6b85d9234f6be0
José María Márquez Crespo <josemaria.marquez@ust.com>
2024-09-03 11:43:43 +0200

    feat: add character and origin entities

---
+++ RickAndMortyApp/RickAndMortyApp/Domain/Entities/Character.swift
@@ -0,0 +1,18 @@
+//
+//  Character.swift
+//  RickAndMortyApp
+//
+//  Created by José María Márquez Crespo on 3/9/24.
+//
+
+import Foundation
+
+struct Character {
+    let id: String
+    let name: String
+    let status: String
+    let species: String
+    let gender: String
+    let origin: CharacterOrigin
+    let image: String
+}
---
+++ RickAndMortyApp/RickAndMortyApp/Domain/Entities/CharacterOrigin.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterOrigin.swift
+//  RickAndMortyApp
+//
+//  Created by José María Márquez Crespo on 3/9/24.
+//
+
+import Foundation
+
+struct CharacterOrigin {
+    let name: String
+}
