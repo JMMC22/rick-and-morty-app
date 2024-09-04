dfd28d951e85a02b8a3c422ebb0996aca9782454
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2024-09-04 10:44:44 +0200

    feat: add character details request to view

--- RickAndMortyApp/RickAndMortyApp/Presentation/Screens/Details/CharacterDetailsViewModel.swift
+++ RickAndMortyApp/RickAndMortyApp/Presentation/Screens/Details/CharacterDetailsViewModel.swift
@@ -9,11 +9,43 @@ import Foundation
 
 class CharacterDetailsViewModel: ObservableObject {
 
+    @Published var serieCharacter: SerieCharacter?
+
     private let id: String
     private let fetchCharacter: FetchCharacter
 
     init(id: String, fetchCharacter: FetchCharacter) {
         self.id = id
         self.fetchCharacter = fetchCharacter
+    }
+
+    func viewDidLoad() async {
+        await fetchCharacter()
+    }
+}
+
+extension CharacterDetailsViewModel {
+
+    private func fetchCharacter() async {
+        let result = await fetchCharacter.execute(id: id)
+
+        switch result {
+        case .success(let serieCharacter):
+            handleFetchCharacterSuccess(serieCharacter: serieCharacter)
+        case .failure(let error):
+            handleFetchCharacterFailure(error: error)
+        }
+    }
+
+    private func handleFetchCharacterSuccess(serieCharacter: SerieCharacter) {
+        print("||SUCCESS|| character: \(serieCharacter.id)")
+
+        DispatchQueue.main.async {
+            self.serieCharacter = serieCharacter
+        }
+    }
+
+    private func handleFetchCharacterFailure(error: AppError) {
+        print("||ERROR|| fetchCharacter error: \(error.localizedDescription)")
     }
 }
