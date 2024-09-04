0f1f72ddf42e56af01370008ae40580a6273c34e
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2024-09-04 09:04:39 +0200

    feat: add fecth characters request to list view

--- RickAndMortyApp/RickAndMortyApp/Presentation/Screens/List/CharactersListView.swift
+++ RickAndMortyApp/RickAndMortyApp/Presentation/Screens/List/CharactersListView.swift
@@ -17,5 +17,8 @@ struct CharactersListView: View {
 
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .task {
+                await viewModel.viewDidLoad()
+            }
     }
 }
--- RickAndMortyApp/RickAndMortyApp/Presentation/Screens/List/CharactersListViewModel.swift
+++ RickAndMortyApp/RickAndMortyApp/Presentation/Screens/List/CharactersListViewModel.swift
@@ -10,8 +10,35 @@ import Foundation
 class CharactersListViewModel: ObservableObject {
 
     private let fetchCharacters: FetchCharacters
+    private var page: Int = 1
 
     init(fetchCharacters: FetchCharacters) {
         self.fetchCharacters = fetchCharacters
+    }
+
+    func viewDidLoad() async {
+        await fetchCharacters(page: page)
+    }
+}
+
+extension CharactersListViewModel {
+
+    func fetchCharacters(page: Int) async {
+        let result = await fetchCharacters.execute(page: page)
+        
+        switch result {
+        case .success(let characters):
+            handleFetchCharactersSuccess(characters: characters)
+        case .failure(let error):
+            handleFetchCharactersFailure(error: error)
+        }
+    }
+
+    private func handleFetchCharactersSuccess(characters: [SerieCharacter]) {
+        print("||SUCCESS|| characters: \(characters.count)")
+    }
+
+    private func handleFetchCharactersFailure(error: AppError) {
+        print("||ERROR|| fetchCharacters error: \(error.localizedDescription)")
     }
 }
