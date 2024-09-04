9d43c206e638a0df2d5229b2c3c99db3c9d68ef5
José María Márquez Crespo <josemaria.marquez@ust.com>
2024-09-04 08:10:51 +0200

    feat: add empty list view and viewmodel

---
+++ RickAndMortyApp/RickAndMortyApp/Presentation/Screens/List/CharactersListView.swift
@@ -0,0 +1,21 @@
+//
+//  CharactersListView.swift
+//  RickAndMortyApp
+//
+//  Created by José María Márquez Crespo on 4/9/24.
+//
+
+import SwiftUI
+
+struct CharactersListView: View {
+
+    @StateObject private var viewModel: CharactersListViewModel
+
+    init(viewModel: CharactersListViewModel) {
+        self._viewModel = StateObject(wrappedValue: viewModel)
+    }
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
---
+++ RickAndMortyApp/RickAndMortyApp/Presentation/Screens/List/CharactersListViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  CharactersListViewModel.swift
+//  RickAndMortyApp
+//
+//  Created by José María Márquez Crespo on 4/9/24.
+//
+
+import Foundation
+
+class CharactersListViewModel: ObservableObject {}
