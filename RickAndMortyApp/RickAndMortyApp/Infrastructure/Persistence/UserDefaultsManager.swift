f4f20a6ef1c5b1ab3f3a9c17d412992f67276f24
José María Márquez Crespo <jmmarquezcrespo@gmail.com>
2024-09-05 07:23:31 +0200

    refactor: delete unused methods on user defaults manager

--- RickAndMortyApp/RickAndMortyApp/Infrastructure/Persistence/UserDefaultsManager.swift
+++ RickAndMortyApp/RickAndMortyApp/Infrastructure/Persistence/UserDefaultsManager.swift
@@ -11,7 +11,6 @@ protocol UserDefaultsManager {
     func get<T>(for key: UserDefaultsKey) -> T?
     func set<T>(_ obj: T, for key: UserDefaultsKey)
     func remove(for key: UserDefaultsKey)
-    func object(for key: UserDefaultsKey) -> Any?
 }
 
 class DefaultUserDefaultsManager: NSObject, UserDefaultsManager {
@@ -33,22 +32,5 @@ class DefaultUserDefaultsManager: NSObject, UserDefaultsManager {
 
     func remove(for key: UserDefaultsKey) {
         self.userDefaults.removeObject(forKey: key.value)
-    }
-
-    func object(for key: UserDefaultsKey) -> Any? {
-        return self.userDefaults.object(forKey: key.value)
-    }
-
-    static func clean() {
-        let keys = UserDefaultsKey.allCases
-        keys.forEach {
-            let excluded = UserDefaultsKey.excludedCases.contains($0)
-
-            if excluded {
-                print("Excluded case: \($0.value)")
-            } else {
-                DefaultUserDefaultsManager.shared.remove(for: $0)
-            }
-        }
     }
 }
